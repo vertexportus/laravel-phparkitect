a2c4b39b61bfe8615442dbb99b8d353408cda384
Daniel Alves <daniel@piksel.tech>
2024-04-30 20:05:47 -0300

    initial commit

---
+++ src/Providers/ArkitectServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace VertexPortus\LaravelArkitect\Providers;
+
+class ArkitectServiceProvider extends ServiceProvider
+{
+	/**
+	 * @return void
+	 */
+	public function register(): void
+	{
+        // TODO
+	}
+
+	/**
+	 * @return void
+	 */
+	public function boot(): void
+	{
+		// TODO
+	}
+}
